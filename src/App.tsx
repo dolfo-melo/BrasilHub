fc335e07ef1c26892f534a166bddfb8f3ffa82cd
Rodolfo Melo <rodolfogameroficial@gmail.com>
2026-02-10 16:10:09 -0300

    Building App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import  api from './services/api.ts'
-import './App.css'
+import './main.css'
 
 export default function App() {
 
@@ -12,9 +12,8 @@ export default function App() {
     try{
       setErro('')
       const response = await api.get(`${uf}?providers=dados-abertos-br,gov,wikipedia`)
-      console.log(response)
-      console.log(response.data)
       setMunicipios(response.data)
+      setUf('')
     }catch{
       setErro('Erro na pesquisa. Tente novamente!!')
       setUf('')
@@ -23,12 +22,32 @@ export default function App() {
 
   return (
     <>
-      <input 
-        value={uf.toUpperCase()}
-        onChange={e => setUf(e.target.value)}
-        placeholder='Digite a sigla do seu estado'
-      />
-      <button onClick={handleSearch}>Pesquisar</button>
+      <div className="container flex collumn justify-center align-center max-w-full h-100">
+        <h1 className="text-5xl">Bem-vindo ao BrasilHub</h1>
+      </div>
+      <div className="container flex collumn justify-center align-center max-w-full h-100">
+        <input 
+          className="w-50 h-10"
+          value={uf.toUpperCase()}
+          onChange={e => setUf(e.target.value)}
+          placeholder='Digite a sigla do seu estado'
+        />
+        <button className="w-50 h-10" onClick={handleSearch}>Pesquisar</button>
+        {erro && <p className='erro'>{erro}</p>}
+      </div>
+      <div className="resultado">
+        <ul>
+          {municipios.map((municipio: any) => (
+            <div className="content flex collumn justify-center align-center max-w-full h-100">
+              <li key={municipio.id}>
+                <h2>{municipio.nome}</h2>
+                <h4>{municipio.codigo_ibge}</h4>
+                <button>Saiba mais</button>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
