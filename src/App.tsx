e9e8ae81fdef693b66278c99463cb72114149d39
Rodolfo Melo <rodolfogameroficial@gmail.com>
2026-02-11 17:00:46 -0300

    Testing react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,82 @@
 import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { CityCard }  from './components/CityCard.tsx'
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react"; 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 import  api from './services/api.ts'
-import { CityCard }  from './components/CityCard.tsx'
 import './main.css'
 
-gsap.registerPlugin(useGSAP,ScrollTrigger);
+gsap.registerPlugin(useGSAP,ScrollTrigger,ScrambleTextPlugin);
 
 export default function App() {
 
   const [uf, setUf] = useState<string>('')
-  const [municipios, setMunicipios] = useState([])
-  const [erro, setErro] = useState<string>('')
+  
+  // React Query para busca e paginação
 
-  // Função buscar cidades a partir do estado
-
-  async function handleSearch() {
-    try{
-      setErro('')
-      const response = await api.get(`${uf}?providers=dados-abertos-br,gov,wikipedia`)
-      setMunicipios(response.data)
-      setUf('')
-    }catch{
-      setErro('Erro na pesquisa. Tente novamente!!')
-      setUf('')
-    }
+  const {data: municipios} = useQuery({
+    queryKey: ['municipios', uf],
+    queryFn: async () => {
+          const response = await api.get(`${uf}?providers=dados-abertos-br,gov,wikipedia`)
+          return response.data
+    },
+    enabled: !!uf, // Somente executa quando uf tem valor válido
+    staleTime: 5 * 60 * 1000, // 5 minutos de cache      
   }
+  )
 
   // Animações GSAP
   useGSAP(() => {
-    
+    gsap.to(".text", {
+      scrambleText: "BrasilHub",
+      chars: "upperAndLowerCase",
+      duration: 2,
+      tweenLenght: true,
+      ease: "power2.inOut",
+      overwrite: "auto",
+    }),
+    gsap.to(".logo", {
+      rotation: 10,
+      duration: 4,
+      ease: "power4.inOut",
+      overwrite: "auto",
+    })
   })
 
   return (
     <>
-      <div className="container flex collumn justify-center align-center max-w-full max-h-full gap-10">
+      <div className="container flex justify-center align-center max-w-full max-h-full gap-100">
         <div className="overflow-hidden">
-          <h1 className=" text-5xl overflow-hidden">Bem-vindo ao BrasilHub</h1>
-          <h3 className=" text-2xl overflow-hidden">Descubra as cidades brasileiras</h3>
+          <div className="content flex flex-col justify-center items-center">
+            <h1 className="text text-5xl overflow-hidden">BrasilHub</h1>
+            <div className="content flex flex-col justify-center items-center gap-3">
+              <h3 className="subtext text-2xl overflow-hidden">Descubra as cidades brasileiras</h3>
+              <input 
+                className="cursor-pointer text-start mt-4 p-2 rounded-md border-5 border-gray-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent w-80 h-15 text-2xl font-bold"
+                value={uf.toUpperCase()}
+                onChange={e => setUf(e.target.value)}
+                placeholder='Digite a sigla do seu estado EX: SP'
+              />
+            </div>
+          </div>
         </div>
-        <img src="../public/logoBrasilHub3.png" alt="logo" width={1500} height={1500}/>
+        <img 
+          src="../public/logoBrasilHub3.png"  
+          className="logo" 
+          alt="logo" 
+          width={1000} 
+          height={1000} 
+          draggable={false} />
       </div>
       
       
       <div className="flex flex-col justify-center items-center max-w-dvw max-h-dvh">
-        <h1 className="text heading-text overflow-hidden">BrasilHub</h1>
-        <div className="flex flex-col justify-center items-center w-dvw h-300">
-          <input 
-            className="w-50 h-10"
-            value={uf.toUpperCase()}
-            onChange={e => setUf(e.target.value)}
-            placeholder='Digite a sigla do seu estado'
-          />
-          <button className="w-50 h-10" onClick={handleSearch}>Pesquisar</button>
-          {erro && <p className='erro'>{erro}</p>}
-        </div>
-      
         <div className="resultado flex flex-col-reverse justify-center align-center max-w-dvw max-h-full">
           <ul>
-            {municipios.map((municipio: any) => (
-              <CityCard key={municipio.id} city={municipio} />
+            {municipios?.map((municipio: any) => (
+              <CityCard key={municipio?.id} city={municipio} />
             ))}
           </ul>
         </div>
