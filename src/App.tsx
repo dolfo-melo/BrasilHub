c35da79a8ef70c205686ce93531df63895efa424
Rodolfo Melo <rodolfogameroficial@gmail.com>
2026-02-12 15:16:36 -0300

    Front-End finished

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
       overwrite: "auto",
     }),
     tl.from(".logo", {
-      xPercent: 100,
+      xPercent: 50,
       opacity: 0.2,
       duration: 1,
       scale: 1.2,
@@ -103,14 +103,14 @@ useGSAP(() => {
 
 return (
     <>
-      <div className="container flex justify-center align-center max-w-full max-h-full gap-100 overflow-hidden">
-        <div className="overflow-hidden">
-          <div className="content flex flex-col justify-center items-center rounded-lg p-10 gap-5">
-            <h1 className="text overflow-hidden text-green-600 text-shadow-md text-shadow-black cursor-pointer"></h1>
-            <div className="mainInput content flex flex-col justify-center items-center gap-3 bg-blue-400 max-h-50 p-10 rounded-4xl max-w-100 ">
-              <h3 className="subtext text-2xl overflow-hidden text-amber-50 font-bold cursor-pointer"></h3>
+      <div className=" container relative min-w-dvw w-full min-h-8/12 flex flex-col-reverse lg:flex-row items-center justify-center gap-60 overflow-hidden">
+        <div className="z-10 w-full lg:w-1/2 flex flex-col items-center lg:items-end transition-all duration-500 overflow-hidden">
+          <div className="z-10 w-screen flex flex-col items-center sm:w-6/12 md:w-8/12 lg:w-12/12 justify-end transition-all duration-500 ease-in-out">
+            <h1 className="text overflow-hidden text-green-600 text-shadow-md text-shadow-black cursor-pointer sm:text-3xl md:text-5xl lg:text-8xl"></h1>
+            <div className="mainInput content flex flex-col justify-start items-center gap-3 bg-blue-400 sm:max-h-50 w-60 text-2xl  lg:max-h-60 p-10 rounded-4xl max-w-100">
+              <h3 className="subtext text-white text-xl md:text-2xl font-bold mb-2 text-center cursor-pointer"></h3>
               <input 
-                className="cursor-pointer text-start mt-4 p-2 rounded-md border-5 border-gray-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent w-80 h-15 text-2xl font-bold"
+                className="w-full h-10 p-4 rounded-xl border-4 border-gray-700 text-center text-2xl font-bold focus:ring-4 focus:ring-yellow-500 outline-none transition-all uppercase"
                 value={uf.toUpperCase()}
                 onChange={e => setUf(e.target.value)}
                 placeholder='Digite a sigla do seu estado EX: SP'
@@ -118,11 +118,13 @@ return (
             </div>
           </div>
         </div>
-        <img 
-          src="../public/logoBrasilHub3.png"  
-          className="logo" 
-          alt="logo"  
-          draggable={false} />
+        <div className="inset-0 z-0 overflow-hidden">
+          <img 
+            src="/logoBrasilHub.png"  
+            className="logo w-4/12 sm: flex justify-end items-end lg:w-10/12 object-contain" 
+            alt="logo"  
+            draggable={false} />
+        </div>
       </div>
       
       {municipios?.length > 0 && (
@@ -132,9 +134,9 @@ return (
           
             {/* Container Resultados */}
             <div className="cityCardWapper w-full max-w-screen max-h-screen flex flex-col items-center">
-              <ul className="grid grid-cols-1 text-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-9xl" ref={gridRef}>
+              <ul className="grid grid-cols-1 text-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 w-9xl" ref={gridRef}>
                 {municipiosPaginados?.map((municipio: any) => (
-                  <li className="">
+                  <li className="sm: items-start h-50">
                     <CityCard key={municipio?.id} city={municipio} />
                   </li>
                 ))}
